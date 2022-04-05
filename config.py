6f939a6917c1053654154ff2e1a6ad00b38a6391
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2022-04-05 19:30:21 +0300

    third

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 
 URL = "https://geekclone.herokuapp.com/"
+URI = "postgres://uiussrcamotndg:f8073d1e812ca9572966b4d3407efeda3ca1685565497278834a92e75543b15f@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/da89dev3296ser"
